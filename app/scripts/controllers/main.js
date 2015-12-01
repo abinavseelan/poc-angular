177c75348e8fc7c155d12179f6884d7d1d62f5b1
Abinav Seelan <abinav.n.seelan@gmail.com>
2015-12-01 15:10:43 +0530

    Typeahead and posts

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,23 @@
  * Controller of the pocAngularApp
  */
 angular.module('pocAngularApp')
-  .controller('MainCtrl',function ($scope) {
+  .controller('MainCtrl',function ($scope,$http) {
 
-  	
+  	$scope.users = {};
+
+  	$scope.getUsers = function(val) {
+	    return $http({
+	    	method: "GET",
+	    	url: "http://localhost:7891/api1/users",
+	    	params: {
+	    		name: val
+	    	}
+	    }).then(function onSuccess(response){
+	    	return response.data.map(function(item){
+	        	return item.first_name;
+	      	});
+	    }, function onError(response){
+	    	console.log(response);
+	    });
+  	};
   });
